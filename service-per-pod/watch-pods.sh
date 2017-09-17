7054f1fdcb087a608a9213ed4f538bfac95b9eac
ted sluis <ted.sluis@gmail.com>
2017-09-17 17:06:31 +0200

    On pod change: remove old routes and add new routes

--- service-per-pod/watch-pods.sh
+++ service-per-pod/watch-pods.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# == Watch Services
-oc get pods --watch --template='{{ .metadata.name}}
+# == Watch pods
+oc get pods --selector="${FILTER}" --watch --template='{{ .metadata.name}}
 ' |while read line
 do
-  echo "== Found new broker: " $line
-  oc get pods | grep -i running 
-  oc get pods | grep -i terminating
+  echo "== Found new pod: " $line
+  oc get pods   --selector="${FILTER}" --template="`cat $TEMPLATE`" |oc create -f -
+  oc get routes --selector="${FILTER}" -o name |egrep -v "`oc get pods --template='{{range .items}}{{if eq .status.phase "Running" }}{{.metadata.name}}|{{end}}{{end}}'`dummy" |xargs --no-run-if-empty -L1 oc delete 
 done
