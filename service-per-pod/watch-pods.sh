220bd74655444e91451207b09e90f885e12668a6
ted sluis <ted.sluis@gmail.com>
2017-09-17 17:25:44 +0200

    watch-pods.sh

--- service-per-pod/watch-pods.sh
+++ service-per-pod/watch-pods.sh
@@ -4,7 +4,9 @@
 oc get pods --selector="${FILTER}" --watch --template='{{ .metadata.name}}
 ' |while read line
 do
-	echo "== Found new pod (filter=${FILTER}, template=${TEMPLATE}): " $line
+ echo "== Found new pod (filter=: '${FILTER}', template: '${TEMPLATE}'): " $line
+ ls -l ${TEMPLATE}
+ cat $TEMPLATE
   oc get pods   --selector="${FILTER}" --template="$(cat $TEMPLATE)" |oc create -f -
   oc get routes --selector="${FILTER}" -o name |egrep -v "`oc get pods --template='{{range .items}}{{if eq .status.phase "Running" }}{{.metadata.name}}|{{end}}{{end}}'`dummy" |xargs --no-run-if-empty -L1 oc delete 
 done
