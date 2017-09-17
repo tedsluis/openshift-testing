a129923a233183fb1e0965319ba9e0424838f037
ted sluis <ted.sluis@gmail.com>
2017-09-17 17:22:48 +0200

    Added echo  and  for debugging.

--- service-per-pod/watch-pods.sh
+++ service-per-pod/watch-pods.sh
@@ -4,7 +4,7 @@
 oc get pods --selector="${FILTER}" --watch --template='{{ .metadata.name}}
 ' |while read line
 do
-  echo "== Found new pod: " $line
-  oc get pods   --selector="${FILTER}" --template="`cat $TEMPLATE`" |oc create -f -
+	echo "== Found new pod (filter=${FILTER}, template=${TEMPLATE}): " $line
+  oc get pods   --selector="${FILTER}" --template="$(cat $TEMPLATE)" |oc create -f -
   oc get routes --selector="${FILTER}" -o name |egrep -v "`oc get pods --template='{{range .items}}{{if eq .status.phase "Running" }}{{.metadata.name}}|{{end}}{{end}}'`dummy" |xargs --no-run-if-empty -L1 oc delete 
 done
